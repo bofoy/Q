904f6e5851e5ec42c3bb06807ea8537f4aa417b0
Dale Shin <dshin192@gmail.com>
2017-05-24 19:57:32 -0400

    Initial commit

---
+++ SpotMe/SpotMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotMe
+//
+//  Created by Dale Shin on 5/24/17.
+//  Copyright © 2017 Dale Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
